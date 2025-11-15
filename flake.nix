087bfd785eea317f9331047aa18df9a27ee73ed0
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-11-15 19:31:23 +0100

    add nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs.python311Packages; [
+          python
+          tqdm
+          numpy
+          biopython
+        ];
+      };
+    }
+  );
+}
